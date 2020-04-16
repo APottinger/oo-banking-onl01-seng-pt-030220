c6948cc42730e020320f883f87af77d5ce99e33f
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-16 20:22:24 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
-
+  
+  attr_accessor :status, :broke, :balance
+  attr_reader :name
+  
+  
+  def initialize(name, balance = 1000, status = "open")
+    @name, @balance, @status = name, balance, status
+  end 
+    
+  def deposit(money)
+    @balance += money
+  end 
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+  def broke
+    @balance = 0 
+  end 
+  
+  def closed
+    @status = "closed"
+  end 
+  
+  def valid?
+    #this is a boolean that returns true or false(y or n) if the balance is greater than 0 and the status is open. To implement this functionality, if statements regarding the instances.
+    status == "open" && balance > 0
+    
+  end 
+  
+  def close_account(balance = 0)
+    self.closed
+  end 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
-  # your code here
+    attr_accessor :sender, :receiver, :status, :amount
+    
+    def initialize(sender, receiver, amount = 50)
+      @sender, @receiver, @amount = sender, receiver, amount
+      @status = "pending"
+    end
+    
+    def valid?
+      sender.valid? && receiver.valid?
+    end
+    
+  def execute_transaction
+    if sender.balance < amount
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+
+    elsif status == "complete"
+      puts "Transaction was already excuted"
+       
+    else
+      sender.balance -= amount 
+      receiver.balance += amount
+      self.status = "complete"
+    end
+  end
+    
+  def reverse_transfer
+    if status == "complete"
+      sender.balance += amount 
+      receiver.balance -= amount 
+      self.status = "reversed"
+    end 
+  end 
+        
+      
+      
 end
