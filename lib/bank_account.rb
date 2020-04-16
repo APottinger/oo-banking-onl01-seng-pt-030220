ace8334e702a48bc644372d574505331d4cc7f8d
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-16 20:23:18 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,36 +1,28 @@
+
 class BankAccount
-  
-  attr_accessor :status, :broke, :balance
+
+  attr_accessor :balance, :status
   attr_reader :name
-  
-  
-  def initialize(name, balance = 1000, status = "open")
-    @name, @balance, @status = name, balance, status
-  end 
-    
-  def deposit(money)
-    @balance += money
-  end 
-  
+
+  def initialize(name, balance=1000)
+    @name = name
+    @balance = balance
+    @status = "open"
+  end
+
+  def deposit(amt)
+    @balance += amt
+  end
+
   def display_balance
     "Your balance is $#{balance}."
   end
-  
-  def broke
-    @balance = 0 
-  end 
-  
-  def closed
-    @status = "closed"
-  end 
-  
+
   def valid?
-    #this is a boolean that returns true or false(y or n) if the balance is greater than 0 and the status is open. To implement this functionality, if statements regarding the instances.
-    status == "open" && balance > 0
-    
-  end 
-  
-  def close_account(balance = 0)
-    self.closed
-  end 
+    @status == "open" && balance > 0
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 end
